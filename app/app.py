c7e422366245a33c8db837b159a88076a528bd95
Tarun Kanti Jena <98694852+tkj2410@users.noreply.github.com>
2023-03-22 19:14:56 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -24,6 +24,17 @@ async def read_item(request: Request, id: str):
 async def handle_options():
     return {"Allow": "POST"}
 
+@app.post("/generate_output")
+async def generate_output(input: dict):
+    
+     Retrieve the input string from the request body
+    input_str = input["input"]
+    
+    TODO: Write code to generate the output string using your logic here
+    output_str = input_str[::-1]
+    
+     Return a JSON response containing the generated output
+    return JSONResponse(content={"output": output_str})
 
 # @app.route("/")
 
