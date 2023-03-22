9ca6b43f47201bd1826657975fef08c84cf9e7df
Tarun Kanti Jena <98694852+tkj2410@users.noreply.github.com>
2023-03-22 19:15:30 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -27,13 +27,13 @@ async def handle_options():
 @app.post("/generate_output")
 async def generate_output(input: dict):
     
-     Retrieve the input string from the request body
+     #Retrieve the input string from the request body
     input_str = input["input"]
     
-    TODO: Write code to generate the output string using your logic here
+    #TODO: Write code to generate the output string using your logic here
     output_str = input_str[::-1]
     
-     Return a JSON response containing the generated output
+    #Return a JSON response containing the generated output
     return JSONResponse(content={"output": output_str})
 
 # @app.route("/")
