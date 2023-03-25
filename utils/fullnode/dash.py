0aeb96f432dfb0952098f0ecf6587a687781b889
samsepi0x0 <samsepi0x0@gmail.com>
2023-03-25 11:18:39 +0530

    Added details for valid addresses

--- utils/fullnode/dash.py
+++ utils/fullnode/dash.py
@@ -17,3 +17,23 @@ def test__check_from_blockcypher():
     # 2
     res = _check_from_blockcypher('XmbWhqnjpGAinNbzrcfsD6ohWpnBYdqJ1D')
     assert res == False
+
+def get_addr_details(addr: str) -> dict:
+    return _get_from_blockcypher(addr)
+    
+
+def _get_from_blockcypher(addr: str) -> dict:
+    if check_addr(addr):
+        res = get(f'https://api.blockcypher.com/v1/dash/main/addrs/{addr}/balance')
+        return json.loads(res.text)
+    else:
+        return {}
+
+def test__get_addr_details_blockcypher():
+    # 1
+    details = get_addr_details('XmbWhqnjpGAinNbzrcfsD6ohWpnBYdqJ1d')
+    assert (not details) == False
+
+    # 2
+    details = get_addr_details('XmbWhqnjpGAinNbzrcfsD6ohWpnBYdqJ1D')
+    assert (not details) == True
