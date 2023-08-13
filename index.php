4176d953af6c8691be66562dcfdcb1177e49b515
varun <74466129+nairvarun@users.noreply.github.com>
2023-08-13 20:30:09 +0530

    .

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    echo "<h1>aa</h1>"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>coin-grep</title>
+</head>
+<body>
+    <h2>coin-grep</h2>
+    <input type="text" id="search">
+    <input type="button" value="🔍" onclick="query()">
+    <br><br>
+    <div id="result"></div>
+    <script src="index.js"></script>
+</body>
+</html>
